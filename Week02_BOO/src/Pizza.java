3cc5f333397d2f2da88d12b3fef937a23309881d
Booji <hyeonji9675@duksung.ac.kr>
2023-09-12 11:41:12 +0900

    WEEK02_1

---
+++ Week02_BOO/src/Pizza.java
@@ -0,0 +1,34 @@
+class Circle {
+    protected int radius;
+    String name;
+
+    public Circle(String name, int r) {
+        radius = r;
+        this.name = name;
+    }
+}
+
+class Pizza0 extends Circle {
+    protected int size; 
+
+    public Pizza0(String name) {
+        super(name);
+    }
+
+    public Pizza0(String name, int size) {
+        super("Pizza", size); 
+        this.size = radius * radius; 
+    }
+
+    public void print() {
+        System.out.println("피자 종류: " + name);
+        System.out.println("피자 넓이: " + size);
+    }
+}
+
+public class Pizza {
+    public static void main(String[] args) {
+        Pizza0 obj = new Pizza0("Pepperoni", 20);
+        obj.print();
+    }
+}
