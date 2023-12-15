c454500b1020b6622d9fa8d68d18f7a8beb1f8cf
JangHyeonji <JangHyeonji@DESKTOP-RKC6T2M>
2023-12-15 18:49:56 +0900

    JAVA 프로그래밍 과제(게임 설계 및 구현)

--- Game_20220713장현지/src/AnimalGame.java
+++ Game_20220713장현지/src/AnimalGame.java
@@ -3,27 +3,32 @@ import javax.swing.*;
 
 public class AnimalGame extends JFrame {
 
-    // 동물 달리기 스레드를 나타내는 내부 클래스
     class MyThread extends Thread {
-        private int[] raceResults; // 경주 결과를 저장하는 배열
-        private JLabel label; // 동물 이미지를 표시하는 JLabel
-        private int x, y; // 동물의 시작 위치
-        private String animalName; // 동물의 이름
+        private int[] raceResults;
+        private JLabel label;
+        private int x, y;
+        private String animalName;
+        private RaceResultPanel resultPanel;
 
         // 동물 달리기 스레드 생성자
-        public MyThread(String animalName, int x, int y, int[] raceResults) {
-            this.animalName = animalName; 
+        public MyThread(String animalName, int x, int y, int[] raceResults, RaceResultPanel resultPanel) {
+            this.animalName = animalName;
             this.x = x;
             this.y = y;
-            this.raceResults = raceResults; 
-            ImageIcon icon = new ImageIcon(animalName + ".jpg"); // 동물 이름으로 이미지 파일 가져옴
+            this.raceResults = raceResults;
+            this.resultPanel = resultPanel;
+            // 동물 이미지 로드
+            ImageIcon icon = new ImageIcon(animalName + ".jpg");
             Image image = icon.getImage();
+            // 이미지 크기 조절
             Image newImage = image.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
             icon = new ImageIcon(newImage);
+            // JLabel을 사용하여 이미지를 표시
             label = new JLabel(icon);
             label.setBounds(x, y, 150, 150);
             add(label);
         }
+
         // 등수 확인 메서드 (동기화 처리)
         private synchronized int getRank() {
             for (int i = 0; i < raceResults.length; i++) {
@@ -44,10 +49,20 @@ public class AnimalGame extends JFrame {
                 if (x >= 650) { // 결승선 도달 조건
                     int rank = getRank(); // 등수 확인
                     System.out.println(rank + "등: " + animalName);
+                    resultPanel.addResult(animalName, rank);
+
+                    // 추가된 부분: 모든 동물이 결승선을 통과하면 최종 우승자 출력
+                    if (resultPanel.allAnimalsFinished()) {
+                        String winner = resultPanel.getWinner();
+                        System.out.println("축하합니다! 우승자는 " + winner + " 입니다!");
+                        System.out.println("**우승자에게는 상품으로 '1년치 식량을 드립니다!**");
+                    }
+
+                    SwingUtilities.invokeLater(() -> label.setVisible(false)); // 경주가 끝난 동물은 화면에서 숨김
                     break;
                 }
                 try {
-                    Thread.sleep(200);
+                    Thread.sleep(200); // 일정 시간 동안 대기
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -55,20 +70,62 @@ public class AnimalGame extends JFrame {
         }
     }
 
+    class RaceResultPanel extends JPanel {
+        private JTextArea resultTextArea;
+        private int finishedCount;
+
+        // 경주 결과를 표시하는 패널 생성자
+        public RaceResultPanel() {
+            resultTextArea = new JTextArea();
+            resultTextArea.setEditable(false);
+            // 스크롤 가능한 JTextArea를 사용하여 결과를 표시
+            JScrollPane scrollPane = new JScrollPane(resultTextArea);
+            scrollPane.setPreferredSize(new Dimension(150, 300));
+            add(scrollPane);
+        }
+
+        // 결과 추가 메서드
+        public void addResult(String animalName, int rank) {
+            resultTextArea.append(rank + "등: " + animalName + "\n");
+            finishedCount++;
+        }
+
+        // 모든 동물이 결승선을 통과했는지 확인하는 메서드
+        public boolean allAnimalsFinished() {
+            return finishedCount == 5;
+        }
+
+        // 최종 우승자를 결정하는 메서드
+        public String getWinner() {
+            // 최종 우승자를 결정하는 로직 추가 (여기서는 간단히 1등으로 도착한 동물 이름을 리턴)
+            for (int i = 0; i < raceResults.length; i++) {
+                if (raceResults[i] == 1) {
+                    return animalNames[i];
+                }
+            }
+            return "";
+        }
+    }
+
+    private String[] animalNames = {"토끼", "강아지", "양", "돼지", "랫서팬더"};
+    private int[] raceResults = new int[5];
+
     // AnimalGame 클래스 생성자
     public AnimalGame() {
         setTitle("동물 달리기 대회");
-        setSize(800, 600);
+        setSize(1000, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        int[] raceResults = new int[5]; // 대회 등수 저장할 배열 생성
+        // 결과를 표시하는 패널 생성
+        RaceResultPanel resultPanel = new RaceResultPanel();
+        resultPanel.setBounds(800, 100, 200, 600);
+        add(resultPanel);
 
-        (new MyThread("토끼", 100, 0, raceResults)).start();
-        (new MyThread("강아지", 100, 100, raceResults)).start();
-        (new MyThread("양", 100, 200, raceResults)).start();
-        (new MyThread("돼지", 100, 300, raceResults)).start();
-        (new MyThread("랫서팬더", 100, 400, raceResults)).start();
+        // 각각의 동물에 대한 스레드 생성 및 시작
+        for (int i = 0; i < animalNames.length; i++) {
+            (new MyThread(animalNames[i], 100, i * 100, raceResults, resultPanel)).start();
+        }
 
         // 결승선을 나타내는 패널
         JPanel finishLinePanel = new JPanel() {
@@ -77,7 +134,7 @@ public class AnimalGame extends JFrame {
                 super.paintComponent(g);
                 Graphics2D g2d = (Graphics2D) g;
                 g2d.setStroke(new BasicStroke(5));
-                g.drawLine(650, 0, 650, 600); 
+                g.drawLine(650, 0, 650, 600);
             }
         };
         finishLinePanel.setBounds(0, 0, 800, 600);
@@ -89,7 +146,7 @@ public class AnimalGame extends JFrame {
     // 메인 메서드
     public static void main(String[] args) {
         System.out.println("[제 1회 동물 달리기 대회]");
-        System.out.println("**1등으로 통과하는 동물에게는 '1년치 식량'을 상품으로 드립니다!**");
+        System.out.println("참가 동물( 토끼 / 강아지 / 양 / 돼지 / 랫서팬더 )");
         System.out.println("----- 대회 결과 -----");
         AnimalGame c = new AnimalGame();
     }
